73210e6b595c09cac565683cbdc4822bf5994946
Benjamin GOEHRY <benjamin.goehry@total.com@MacBook-Pro-de-Benjamin.local>
2020-09-11 11:30:34 +0200

    implement 15 15

--- score.py
+++ score.py
@@ -2,16 +2,26 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Player:
+class Players:
     player_1: int = 0
     player_2: int = 1
 
 
+@dataclass
+class Scores:
+    score_0_0: tuple = (0, 0)
+    score_15_0: tuple = (15, 0)
+    score_0_15: tuple = (0, 15)
+    score_15_15: tuple = (15, 15)
+
+
 def get_score(scoring_player: int = None, score: tuple = None) -> tuple:
-    if scoring_player == Player.player_1:
-        return 15, 0
-    elif scoring_player == Player.player_2:
-        if score == (15, 0):
-            return 15, 15
-        return 0, 15
-    return 0, 0
+    if scoring_player == Players.player_1:
+        if score == Scores.score_0_15:
+            return Scores.score_15_15
+        return Scores.score_15_0
+    elif scoring_player == Players.player_2:
+        if score == Scores.score_15_0:
+            return Scores.score_15_15
+        return Scores.score_0_15
+    return Scores.score_0_0
--- test_score.py
+++ test_score.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-from score import get_score, Player
+from score import get_score, Players, Scores
 
 
 class Test(TestCase):
     def test_get_score_start_with_0_0(self):
         # Given
-        expected_score: tuple = (0, 0)
+        expected_score: tuple = Scores.score_0_0
 
         # When
         score: tuple = get_score()
@@ -16,8 +16,8 @@ class Test(TestCase):
 
     def test_get_score_when_player_1_score_once(self):
         # Given
-        expected_score: tuple = (15, 0)
-        scoring_player: int = Player.player_1
+        expected_score: tuple = Scores.score_15_0
+        scoring_player: int = Players.player_1
 
         # When
         score: tuple = get_score(scoring_player)
@@ -27,8 +27,8 @@ class Test(TestCase):
 
     def test_get_score_when_player_2_score_once(self):
         # Given
-        expected_score: tuple = (0, 15)
-        scoring_player: int = Player.player_2
+        expected_score: tuple = Scores.score_0_15
+        scoring_player: int = Players.player_2
 
         # When
         score: tuple = get_score(scoring_player)
@@ -38,9 +38,21 @@ class Test(TestCase):
 
     def test_get_score_when_player_1_lead_by_15_0_player_2_scores(self):
         # Given
-        expected_score: tuple = (15, 15)
-        scoring_player: int = Player.player_2
-        current_score: tuple = (15, 0)
+        expected_score: tuple = Scores.score_15_15
+        scoring_player: int = Players.player_2
+        current_score: tuple = Scores.score_15_0
+
+        # When
+        score: tuple = get_score(scoring_player, current_score)
+
+        # Then
+        self.assertEqual(expected_score, score)
+
+    def test_get_score_when_player_2_lead_by_0_15_player_1_scores(self):
+        # Given
+        expected_score: tuple = Scores.score_15_15
+        scoring_player: int = Players.player_1
+        current_score: tuple = Scores.score_0_15
 
         # When
         score: tuple = get_score(scoring_player, current_score)
