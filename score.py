520f4da243cde18dbd2c136ca536953208671658
a.mosca <a.mosca@octo.com>
2020-09-11 11:13:52 +0200

    implement 15/15

--- score.py
+++ score.py
@@ -1,6 +1,17 @@
-def get_score(scoring_player: int = None) -> tuple:
-    if scoring_player == 0:
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    player_1: int = 0
+    player_2: int = 1
+
+
+def get_score(scoring_player: int = None, score: tuple = None) -> tuple:
+    if scoring_player == Player.player_1:
         return 15, 0
-    elif scoring_player == 1:
+    elif scoring_player == Player.player_2:
+        if score == (15, 0):
+            return 15, 15
         return 0, 15
     return 0, 0
--- test_score.py
+++ test_score.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from score import get_score
+from score import get_score, Player
 
 
 class Test(TestCase):
@@ -12,26 +12,39 @@ class Test(TestCase):
         score: tuple = get_score()
 
         # Then
-        self.assertEqual(score, expected_score)
+        self.assertEqual(expected_score, score)
 
     def test_get_score_when_player_1_score_once(self):
         # Given
         expected_score: tuple = (15, 0)
-        scoring_player: int = 0
+        scoring_player: int = Player.player_1
 
         # When
         score: tuple = get_score(scoring_player)
 
         # Then
-        self.assertEqual(score, expected_score)
+        self.assertEqual(expected_score, score)
 
     def test_get_score_when_player_2_score_once(self):
         # Given
         expected_score: tuple = (0, 15)
-        scoring_player: int = 1
+        scoring_player: int = Player.player_2
 
         # When
         score: tuple = get_score(scoring_player)
 
         # Then
-        self.assertEqual(score, expected_score)
+        self.assertEqual(expected_score, score)
+
+    def test_get_score_when_player_1_lead_by_15_0_player_2_scores(self):
+        # Given
+        expected_score: tuple = (15, 15)
+        scoring_player: int = Player.player_2
+        current_score: tuple = (15, 0)
+
+        # When
+        score: tuple = get_score(scoring_player, current_score)
+
+        # Then
+        self.assertEqual(expected_score, score)
+
